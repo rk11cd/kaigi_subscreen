ac2f5876a2ac8f3cb3df7c45d8538ff84c403920
june29 <june29.jp@gmail.com>
2011-07-02 16:08:40 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rake', '0.8.7'
+
+gem 'rails', '3.1.0.rc4'
+
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'haml'
+gem 'coffee-script'
+gem 'uglifier'
+
+group :development do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
+  gem 'therubyracer-heroku'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subscreen::Application.config.secret_token = 'b329fb406ab46afef16c57741b3e158cb904cfc2fe78de3ede962ea7a3c5ecef8fbce62b099b56d027bb3297aa47c0aff2c8f1eaf67b193eb3d748256f88aefb'
