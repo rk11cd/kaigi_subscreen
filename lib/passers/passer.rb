afa50a1bebb3704da728ee19d4fb6c654fe55741
kei <kei.shiratsuchi@gmail.com>
2011-07-10 02:11:02 +0900

    rename label to event

--- lib/passers/passer.rb
+++ lib/passers/passer.rb
@@ -7,16 +7,16 @@ require "configatron"
 load File.join(File.dirname(__FILE__), "..", "..", "config", "initializers", "0_load_config.rb")
 
 class Passer
-  def initialize(channel, label)
+  def initialize(channel, event)
     Pusher.app_id = configatron.pusher.app_id
     Pusher.key    = configatron.pusher.key
     Pusher.secret = configatron.pusher.secret
 
     @channel = channel
-    @label   = label
+    @event   = event
   end
 
   def pass(data)
-    Pusher[@channel].trigger!(@label, data)
+    Pusher[@channel].trigger!(@event, data)
   end
 end
