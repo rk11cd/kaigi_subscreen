dbc40ac16f847367231b9419e7c53dbd6752b834
june29 <june29.jp@gmail.com>
2011-07-14 00:39:30 +0900

    save tweets in file

--- lib/passers/twitter-search-passer.rb
+++ lib/passers/twitter-search-passer.rb
@@ -16,11 +16,24 @@ class TwitterSearchPasser < Passer
   end
 
   def start
+    prev    = nil
+    current = Time.now.strftime("%Y%m%d%H")
+    logfile = open(File.join(File.dirname(__FILE__), "..", "..", "db", "tweets", "#{@query}.#{current}"), "a+")
+
     uri = URI.parse("http://#{@auth}@stream.twitter.com/1/statuses/filter.json?track=#{URI.encode(@query)}")
     Yajl::HttpStream.get(uri) do |tweet|
       unless @ignore.include? tweet["user"]["screen_name"]
         p tweet
         pass(tweet)
+
+        current = Time.now.strftime("%Y%m%d%H")
+        unless current == prev
+          logfile = open(File.join(File.dirname(__FILE__), "..", "..", "db", "tweets", "#{@query}.#{current}"), "a+")
+        end
+
+        logfile.puts tweet
+
+        prev = current
       end
     end
   end
