1ee1cb2f488aa7d44c31bb2fc60d1d445cb350fb
kei <kei.shiratsuchi@gmail.com>
2011-07-16 02:15:55 +0900

    remove unuse setting

--- config/initializers/0_load_config.rb
+++ config/initializers/0_load_config.rb
@@ -10,17 +10,6 @@ if configatron.pusher.secret.nil?
   configatron.pusher.secret = ENV['PUSHER_SECRET']
 end
 
-if configatron.twitter.username.nil?
-  configatron.twitter.username = ENV['TWITTER_USERNAME']
-end
-if configatron.twitter.password.nil?
-  configatron.twitter.password = ENV['TWITTER_PASSWORD']
-end
-if configatron.twitter.ignore.nil?
-  unless ENV['TWITTER_IGNORE'].nil?
-    configatron.twitter.password = ENV['TWITTER_IGNORE'].split(",")
-  end
-end
 if configatron.basic_authentication.username.nil?
   configatron.basic_authentication.username = ENV['BASIC_USERNAME']
 end
